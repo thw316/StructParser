f165d6a5bbdd9089b2524a72c6d6c108493f5df7
timo.wang <timo.wang@siliconmotion.com>
2019-08-25 12:13:07 +0800

    [master] Restructure parser to support multi dimension array.

--- StructParser.py
+++ StructParser.py
@@ -1,29 +1,20 @@
 import csv
 from os import listdir
-from os.path import splitext, basename
 from thwmodule.thwhex import THWHex
-
 # Settings
 configfolder = 'config'
 
 #function
+# getValue return string
 def getValue(string):  # return string of Value name
-    l = string.replace('<', '>').split('>')
-    if (len(l) >= 2):
-        return l[1]
-    else:
-        return string
+    return string.replace('<', '').replace('>', '').split('"')[0]
 
-# return [type name, type count], if type count ~= 0, it is an array
+# getTpye return [type name, array lens] for array. return [type name] for single value.
 def getType(string):
-    # Trans FlashFifo[255] to ['FlashFifo', '255', '']
-    #  # get structName and structLens from [structName, structLens, '']
-    l = string.replace('[', ']').split(']')[0:2]
-    if (len(l) == 1) :
-        return (l[0], 0)
-    else:
-        return (l[0], int(l[1]))
+    lOut = string.replace(']','').split('[')
+    return [lOut[0]] + list(map(int, lOut[1:]))
 
+# getByteCnt return byte count
 def getByteCnt(string):
     if (string == 'unsigned char'):
         return 1
@@ -33,122 +24,145 @@ def getByteCnt(string):
         return 4
     else:
         print("error getByteCnt")
+        exit(1)
 
+# define list
 def returnList(name, offset, bytecnt, arrlen):
     if type(name) is not str:
         print("error returnList name is not str")
+        exit(1)
     
     if type(offset) is not int:
         print("error returnList offset is not int")
+        exit(1)
     
     if type(bytecnt) is not int:
         print("error returnList bytecnt is not int")
+        exit(1)
 
-    if type(arrlen) is not int:
+    if type(arrlen) is not list:
         print("error returnList arrlen is not int")
+        exit(1)
     
     return [name, offset, bytecnt, arrlen]
 
+# return hex value from dynamic offset with byte count
 def getHexByteLen(hexIIn, idx, byteLen):
     return '0x' + ''.join(['%02x' % hexIIn(idx+idxByte) for idxByte in reversed(range(byteLen))])
+
+def multiplyList(myList) : 
+    # Multiply elements one by one 
+    result = 1
+    for x in myList: 
+         result = result * x  
+    return result
+
+def listDim2StrDim(lIn):
+    string = ''
+    for e in lIn:
+        string = string + '[%s]' % (str(e))
+    return string
 
 # main
 if __name__ == '__main__':
 
-    # temp var
-    templist = ''
-    tmpArrLen = 0
+    while True:
+        # handle input
+        binPath = input("Input binary path (.bin/.hex): ")
+        print("")
+        
+        configNames = listdir(configfolder)
+        print ("0: load other config")
+        for i in range(len(configNames)):
+            print ("%d: %s" % (i+1, configNames[i]))
 
-    # handle input
-    binPath = input("Input binary path (.bin/.hex): ")
-    
-    configNames = listdir(configfolder)
-    print ("0: load other config")
-    for i in range(len(configNames)):
-        print ("%d: %s" % (i+1, configNames[i]))
+        i = int(input("Select above config file: "))
 
-    i = int(input("Select above config file: "))
+        if (0<i) and (i<=len(configNames)): # select from configfolder
+            cfgfile = '%s/%s' % (configfolder, configNames[i-1])
+        else:
+            cfgfile = input("Input config path: ")
 
-    if (0<i) and (i<=len(configNames)): # select from configfolder
-        cfgfile = '%s/%s' % (configfolder, configNames[i-1])
-    else:
-        cfgfile = input("Input config path: ")
+        print ("Load config file: %s" % cfgfile)
+        print("")
 
-    print ("Load config file: %s" % cfgfile)
+        # struct var
+        structVarName = '' # get from first row
+        structName = '' # get from first row
+        structLens = 0 # get from first row
+        structBaseAddr = 0xFFFFFFFF # get from first row
 
-    # struct var
-    structVarName = '' # get from first row
-    structName = '' # get from first row
-    structLens = 0 # get from first row
-    structBaseAddr = 0xFFFFFFFF # get from first row
+        structFormat = [] # main format
 
-    structFormat = [] # main format
+        structSize = 0 # get from struct[1] addr - struct[0] addr
 
-    structSize = 0 # get from struct[1] addr - struct[0] addr
+        with open(cfgfile, 'r', newline='', encoding='utf-8') as csvf:
 
-    with open(cfgfile, newline='') as csvf:
-        rows = csv.DictReader(csvf)
+            rows = csv.DictReader(csvf)
 
-        firstRow = True
-        for row in rows:
+            stage = 1
+            for row in rows:
 
-            if firstRow:
-                structVarName = row['Expression']
-                firstRow = False
+                if stage == 1: # found array of structs (AoS)
+                    if getValue(row['Value']) == 'array': # get info of AoS
+                        structVarName = row['Expression']
+                        structBaseAddr = int(row['Location'], 16)
+                        tempList = getType(row['Type'])
+                        if len(tempList) != 2:
+                            print("error array of struct not support")
+                            exit(1)
+                        structName = tempList[0]
+                        structLens = tempList[1]
+                        stage = 2
 
-            if tmpArrLen != 0:
-                tmpArrLen = tmpArrLen - 1
-                continue
+                elif stage == 2: # found first element of AoS
+                    if (getValue(row['Value']) == 'struct'):
+                        if (row['Expression'] == '[0]') and (int(row['Location'],16) == structBaseAddr) and (getType(row['Type']) == [structName]):
+                            stage = 3
 
-            elif (getValue(row['Value']) == 'array'):
+                elif stage == 3: # construct structFormat
+                    if (getValue(row['Value']) == 'struct'):
+                        if (row['Expression'] == '[1]') and (getType(row['Type']) == [structName]): # found second element of AoS
+                            structSize = int(row['Location'], 16) - structBaseAddr
+                            break # break rows loop to complete config parsing
+                        #else:
+                            # do nothing... need user expand struct inside AoS
+                    elif (getValue(row['Value']) == 'array'):
+                        tempList = getType(row['Type'])
+                        arrType = tempList[0]
+                        arrLens = tempList[1:]
+                        structFormat.append(returnList(row['Expression']+listDim2StrDim(arrLens), int(row['Location'], 16)-structBaseAddr, getByteCnt(arrType), arrLens))
+                    elif ('[' not in row['Expression']):
+                        structFormat.append(returnList(row['Expression'], int(row['Location'], 16)-structBaseAddr, getByteCnt(getType(row['Type'])[0]), [1]))
 
-                if (row['Expression'] == structVarName): # first line get struct info
-                    [structName, structLens] = getType(row['Type'])
-                    structBaseAddr = int(row['Location'], 16)
-                else:
-                    tmpArrLen = getType(row['Type'])[1]
-                    templist = returnList(row['Expression'], int(row['Location'], 16)-structBaseAddr, getByteCnt(getType(row['Type'])[0]), tmpArrLen)
-
-            elif (getValue(row['Value']) == 'struct'):
-                
-                if (getType(row['Type'])[0] == structName): # meet struct line again
-                    if (row['Expression'] == '[0]'): # second line
-                        continue
-                    elif (row['Expression'] == '[1]'): # next element of struct array, end of parsing
-                        structSize = int(row['Location'], 16) - structBaseAddr
-                        break
-
-            else:
-                templist = returnList(row['Expression'], int(row['Location'], 16)-structBaseAddr, getByteCnt(getType(row['Type'])[0]), tmpArrLen)
-            
-            if templist:
-                structFormat.append(templist)
-                templist = ''
-
-    # print(structName, structLens, structBaseAddr, structSize)
-    # for l in structFormat:
-    #     print(l)
-
-    binIn = THWHex(binPath)
-
-    with open(binPath+'.csv', 'w', newline='') as csvf:
-        fieldnames = ['ptr', 'ptr(hex)']
+        print("Name: %s, Lens: %s, Base Addr: %s, Size %s" % (structName, structLens, structBaseAddr, structSize))
+        print("Struct format: [name, offset, byte cnt, array lens]")
         for l in structFormat:
-            fieldnames.append(l[0])
-        writer = csv.DictWriter(csvf, fieldnames=fieldnames)
-        writer.writeheader()
+            print(l)
+        print("")
 
-        for idx in range(0,structLens):
-            dic = {}
-            dic = {'ptr': idx, 'ptr(hex)': hex(idx)}
+        binIn = THWHex(binPath)
 
+        with open(binPath+'.csv', 'w', newline='', encoding='utf-8') as csvf:
+            fieldnames = ['ptr', 'ptr(hex)']
             for l in structFormat:
-                # print(idx, l)
-                if l[3]:
-                    dic[l[0]] = ','.join([getHexByteLen(binIn, idx*structSize+l[1]+idx2, l[2]) for idx2 in range(l[3])])
-                else:
-                    dic[l[0]] = getHexByteLen(binIn, idx*structSize+l[1], l[2])
+                fieldnames.append(l[0])
+            writer = csv.DictWriter(csvf, fieldnames=fieldnames)
+            writer.writeheader()
 
-            # print(dic)
-            writer.writerow(dic)
-            dic.clear()
+            for idx in range(0,structLens):
+                dic = {}
+                dic = {'ptr': idx, 'ptr(hex)': hex(idx)}
+
+                for l in structFormat:
+                    if l[3] != [1]:
+                        dic[l[0]] = ','.join([getHexByteLen(binIn, idx*structSize+l[1]+idx2, l[2]) for idx2 in range(multiplyList(l[3]))])
+                    else:
+                        dic[l[0]] = getHexByteLen(binIn, idx*structSize+l[1], l[2])
+
+                # print(dic)
+                writer.writerow(dic)
+                dic.clear()
+
+        print("Parsing finish....")
+        print("")
